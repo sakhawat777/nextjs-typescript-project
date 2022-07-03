a4c11bc528a75a95791dd2fd3afec002d9b0058e
Md. Sakhawat Hossain <biplobbms@gmail.com>
2022-07-03 17:10:05 +0600

    Get API Done

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ce0f12d7881e961f"; }
+/******/ 		__webpack_require__.h = function() { return "373bb64c8fb69782"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
